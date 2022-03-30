4597bd0ce9375440f3636480f7fa6ca0e525bc8b
Brishan King <brishan.king@gmail.com>
2022-03-30 16:44:19 -0700

    Added poppout text feature for descriptions that were too long to fit in data cell

--- src/pages/LinksPage/LinksPage.jsx
+++ src/pages/LinksPage/LinksPage.jsx
@@ -1,7 +1,120 @@
 import './LinksPage.scss';
 import { DataGrid, GridColDef, GridRenderCellParams } from '@mui/x-data-grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Popper from '@mui/material/Popper';
+import * as React from 'react';
 
-const columns: GridColDef[] = [
+
+//
+// Much of the below code is adapted from example from MUI documentation,
+// https://mui.com/components/data-grid/columns/
+// 
+function isOverflown(element) {
+  return (
+    element.scrollHeight > element.clientHeight ||
+    element.scrollWidth > element.clientWidth
+  );
+}
+
+const GridCellExpand = React.memo(function GridCellExpand( props ) {
+  const { width, value } = props;
+  const wrapper = React.useRef(null);
+  const cellDiv = React.useRef(null);
+  const cellValue = React.useRef(null);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [showFullCell, setShowFullCell] = React.useState(false);
+  const [showPopper, setShowPopper] = React.useState(false);
+
+  const handleMouseEnter = () => {
+    const isCurrentlyOverflown = isOverflown(cellValue.current);
+    setShowPopper(isCurrentlyOverflown);
+    setAnchorEl(cellDiv.current);
+    setShowFullCell(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowFullCell(false);
+  };
+
+  React.useEffect(() => {
+    if (!showFullCell) {
+      return undefined;
+    }
+
+    function handleKeyDown(nativeEvent) {
+      // IE11, Edge (prior to using Bink?) use 'Esc'
+      if (nativeEvent.key === 'Escape' || nativeEvent.key === 'Esc') {
+        setShowFullCell(false);
+      }
+    }
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [setShowFullCell, showFullCell]);
+
+  return (
+    <Box
+      ref={wrapper}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      sx={{
+        alignItems: 'center',
+        lineHeight: '24px',
+        width: 1,
+        height: 1,
+        position: 'relative',
+        display: 'flex',
+      }}
+    >
+      <Box
+        ref={cellDiv}
+        sx={{
+          height: 1,
+          width,
+          display: 'block',
+          position: 'absolute',
+          top: 0,
+        }}
+      />
+      <Box
+        ref={cellValue}
+        sx={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}
+      >
+        {value}
+      </Box>
+      {showPopper && (
+        <Popper
+          open={showFullCell && anchorEl !== null}
+          anchorEl={anchorEl}
+          style={{ width, marginLeft: -17 }}
+        >
+          <Paper
+            elevation={1}
+            style={{ minHeight: wrapper.current.offsetHeight - 3 }}
+          >
+            <Typography variant="body2" style={{ padding: 8 }}>
+              {value}
+            </Typography>
+          </Paper>
+        </Popper>
+      )}
+    </Box>
+  );
+});
+
+function renderCellExpand(params) {
+  return (
+    <GridCellExpand value={params.value || ''} width={params.colDef.computedWidth} />
+  );
+}
+
+
+const columns = [
   {
     field: 'title', 
     headerName: 'Title',
@@ -11,7 +124,7 @@ const columns: GridColDef[] = [
     field: 'url', 
     headerName: 'URL',
     width: 200,
-    renderCell: (params: GridRenderCellParams) => (
+    renderCell: (params) => (
       <a href={params.value} target='_blank'>
         {params.value}
       </a>
@@ -21,6 +134,7 @@ const columns: GridColDef[] = [
     field: 'description', 
     headerName: 'Description',
     width: 200,
+    renderCell: renderCellExpand
   },
   {
     field: 'category', 
@@ -34,6 +148,12 @@ const columns: GridColDef[] = [
   },
 ]
 
+
+
+
+//
+// Renders MUI data grid for a searchable library of links
+//
 function LinksPage({linksList}) {
 
   return (
