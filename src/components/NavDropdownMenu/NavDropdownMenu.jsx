9374e8b4c99a6f17e45676437742f3eb983bf655
Brishan King <brishan.king@gmail.com>
2022-04-03 16:38:45 -0700

    Line change in NavDropdownMenu

--- src/components/NavDropdownMenu/NavDropdownMenu.jsx
+++ src/components/NavDropdownMenu/NavDropdownMenu.jsx
@@ -1,24 +1,25 @@
 import "./NavDropdownMenu.scss";
 import NavDropdownItem from "../NavDropdownItem/NavDropdownItem";
 
+
 function NavDropdownMenu({open, setOpen, theme}) {
   return (
-    <div
-      className={`dropdown ${open ? "dropdown--active" : ""}${theme === 'dark' ? " dropdown--dark" : ""}`}
-    >
-      <NavDropdownItem to="/linklibrary"
-        title="Link Library"
-        theme={theme}
-        open={open}
-        setOpen={setOpen}
-      />
-      <NavDropdownItem to="/settings/background"
-        title="Settings"
-        theme={theme}
-        open={open}
-        setOpen={setOpen}
-      />
-    </div>
+      <div
+        className={`dropdown ${open ? "dropdown--active" : ""}${theme === 'dark' ? " dropdown--dark" : ""}`}
+      >
+        <NavDropdownItem to="/linklibrary"
+          title="Link Library"
+          theme={theme}
+          open={open}
+          setOpen={setOpen}
+        />
+        <NavDropdownItem to="/settings/background"
+          title="Settings"
+          theme={theme}
+          open={open}
+          setOpen={setOpen}
+        />
+      </div> 
   )
 }
 
